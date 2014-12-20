8ae186502c932c49304e4a875542819f5936e0ba
Rafał Rzepecki <divided.mind@gmail.com>
2014-12-20 04:36:51 +0100

    Ruby 1.8 deprecation notice
    As per issue #79

--- lib/sequel_rails/version.rb
+++ lib/sequel_rails/version.rb
@@ -1,3 +1,11 @@
 module SequelRails
   VERSION = '0.9.7'
 end
+
+if RUBY_VERSION < '1.9'
+  warn <<-NOTE
+    NOTE: Support for Ruby 1.8 in sequel-rails is deprecated and will be
+    dropped in a future release. If you really rely on it please complain at
+    http://git.io/WgfgZQ to delay the inevitable.
+  NOTE
+end
